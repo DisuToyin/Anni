98012a01e5417c95231e4bacb0e5d4592e156c3a
DisuToyin <disujt@gmail.com>
2024-04-22 10:46:13 +0100

    x

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // DOM
-const swiper = document.querySelector('#swiper');
-const like = document.querySelector('#like');
-const dislike = document.querySelector('#dislike');
+const swiper = document.querySelector("#swiper");
+const like = document.querySelector("#like");
+const dislike = document.querySelector("#dislike");
 
 // constants
 const urls = [
-  'https://source.unsplash.com/random/1000x1000/?sky',
-  'https://source.unsplash.com/random/1000x1000/?landscape',
-  'https://source.unsplash.com/random/1000x1000/?ocean',
-  'https://source.unsplash.com/random/1000x1000/?moutain',
-  'https://source.unsplash.com/random/1000x1000/?forest'
+  "./images/MD5.jpeg",
+  "./images/MD4.jpeg",
+  "./images/MD3.jpeg",
+  "./images/MD2.jpeg",
+  "./images/MD1.jpeg",
 ];
 
 // variables
@@ -21,20 +21,20 @@ function appendNewCard() {
     imageUrl: urls[cardCount % 5],
     onDismiss: appendNewCard,
     onLike: () => {
-      like.style.animationPlayState = 'running';
-      like.classList.toggle('trigger');
+      like.style.animationPlayState = "running";
+      like.classList.toggle("trigger");
     },
     onDislike: () => {
-      dislike.style.animationPlayState = 'running';
-      dislike.classList.toggle('trigger');
-    }
+      dislike.style.animationPlayState = "running";
+      dislike.classList.toggle("trigger");
+    },
   });
   swiper.append(card.element);
   cardCount++;
 
-  const cards = swiper.querySelectorAll('.card:not(.dismissing)');
+  const cards = swiper.querySelectorAll(".card:not(.dismissing)");
   cards.forEach((card, index) => {
-    card.style.setProperty('--i', index);
+    card.style.setProperty("--i", index);
   });
 }
 
@@ -42,4 +42,3 @@ function appendNewCard() {
 for (let i = 0; i < 5; i++) {
   appendNewCard();
 }
-
